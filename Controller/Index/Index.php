d56b21d5268d2be81578f1f91779f63a4c07b71a
Marko Ambrožič <marko@webam.si>
2022-03-27 15:25:29 +0200

    Made modifications for new csrf validation approach

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace OneO\Shop\Controller\Index;
 
-use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\App\CsrfAwareActionInterface;
+use Magento\Framework\App\Request\InvalidRequestException;
+use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Controller\Result\JsonFactory;
 use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Phrase;
 
-class Index implements HttpPostActionInterface
+class Index implements CsrfAwareActionInterface
 {
     private JsonFactory $jsonFactory;
 
@@ -24,10 +27,43 @@ class Index implements HttpPostActionInterface
      */
     public function execute()
     {
+
+
         $result = $this->jsonFactory->create();
         $data = ['message' => 'Hello world!'];
 
         return $result->setData($data);
+    }
+
+    /**
+     * Create exception in case CSRF validation failed.
+     * Return null if default exception will suffice.
+     *
+     * @param RequestInterface $request
+     *
+     * @return InvalidRequestException|null
+     */
+    public function createCsrfValidationException(
+        RequestInterface $request
+    ): ?InvalidRequestException {
+        if (!$request) {
+            return new InvalidRequestException(
+                $this->response,
+                [new Phrase($this->message)]
+            );
+        }
+    }
+
+    /**
+     * Perform custom request validation.
+     * Return null if default validation is needed.
+     *
+     * @param RequestInterface $request
+     *
+     * @return bool|null
+     */
+    public function validateForCsrf(RequestInterface $request): ?bool {
+        return true;
     }
 }
 
